afd7619fff51d9763e6d1f990c750d64921d7379
the-macson <ak9991927952@gmail.com>
2021-07-16 00:21:19 +0530

    day 3rd assignment

--- daythree/src/App.js
+++ daythree/src/App.js
@@ -9,20 +9,42 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Button } from 'reactstrap';
 
 
-const tiktacArray = new Array(9).fill("A")
+const a = new Array(9).fill("A")
 const App=()=> {
     let[isCross,setIsCross]= useState(true)
     let [winMessage,setWinMessage]=useState("")
     const playAgain =()=>{
           setIsCross(true)
           setWinMessage("")
-          tiktacArray.fill("")
+          a.fill("")
     }
     const findWinner=()=>{
-      // i am not able to complete logic 
-
+      if(a[0]==a[1] && a[0]== a[2] && a[0]!= ""){
+          setWinMessage(a[0]+ " has won")
+      }
+      else if(a[3]==a[4] && a[3]== a[5] && a[3]!=""){
+          setWinMessage(a[3] + " has won")
+      }
+    else if(a[6]== a[7] && a[6]== a[8] && a[6]!=""){
+      setWinMessage(a[6]+" has won")
     }
-  return (
+    else if (a[0]==a[3] && a[0] == a[6] && a[0]!=""){
+      setWinMessage(a[0]+" has won")
+    }
+    else if(a[1]== a[4] && a[1]==a[7] && a[1] !=""){
+      setWinMessage(a[1]+" has won")
+    }
+    else if(a[2]==a[5] && a[2]==a[8] && a[2] !=""){
+      setWinMessage(a[2]+" has won")
+    }
+    else if(a[2]==a[4] && a[2] == a[6] && a[2] !=""){
+       setWinMessage(a[2]+" has won")
+    }
+    else if(a[0] == a[4] && a[0] == a[8] && a[0]!=""){
+      setWinMessage(a[0]+" has won")
+    }
+  }
+    return (
     <>
        <Icon choice="cross"/>
     </>
