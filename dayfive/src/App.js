0a11513046f01c5bee0edc996fb798789448285f
the-macson <ak9991927952@gmail.com>
2021-07-17 13:11:29 +0530

    day five

--- dayfive/src/App.js
+++ dayfive/src/App.js
@@ -1,12 +1,22 @@
-// import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import NavBar from './Components/Navbar'
+import HeroSection from './Components/HeroSection'
+import FeatureSection from './Components/FeatureSection'
+import CardSection from './Components/CardSection'
+import FooterSection from './Components/FooterSection'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div>
+        <NavBar />
+        <FeatureSection />
+        <HeroSection />
+        <CardSection />
+        <FooterSection />
+
     </div>
-  );
+  )
 }
 
 export default App;
